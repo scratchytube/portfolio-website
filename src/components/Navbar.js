8931e7ffd2b7b7b755e5e92a7ac621c81d83aa27
Brian Sahota <bpsahota@gmail.com>
2021-07-30 09:57:09 -0400

    navbar looks decent

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,102 +1,196 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Divide as Hamburger } from 'hamburger-react'
 import { Link, animateScroll as scroll } from "react-scroll";
-import { FaLinkedin, FaGithub  } from 'react-icons/fa';
+import { FaLinkedin, FaGithub, FaBars  } from 'react-icons/fa';
 
 const Navbar = () => {
     return (
         <Wrapper>
-            <NavContainer>
-                <NavHeader>
-                    <Logo>Brian Sahota</Logo>
-                    <MobileBurger>
-                        <Hamburger size={20}/>
-                    </MobileBurger>
-                </NavHeader>
-                <LinksContainer>
-                    <NavUl>
+            <div className="nav-container">
+                <div className="nav-header">
+                    <h3>Brian Sahota</h3>
+                    <button className="nav-toggle">
+                        <FaBars />
+                    </button>
+                </div>
+                <div className="links-container">
+                    <ul className="links">
                         <NavLink to='portfolio' 
-                        spy={true} smooth={true}
-                        offset={-80}duration={500}
-                        >Projects</NavLink
-                        >
-                        <NavLink to='about'
-                        spy={true} smooth={true}
-                        offset={-80}duration={500}
-                        >About</NavLink
-                        >
-                        <NavLink to='contact'
-                        spy={true} smooth={true}
-                        offset={-80}duration={500}
-                        >Contact</NavLink
-                        >
-                    </NavUl>
-                </LinksContainer>
-                <SocialIcons>
-                     <li>
+                            spy={true} smooth={true}
+                            offset={-80}duration={700}
+                            >Projects</NavLink>
+
+                            <NavLink to='about'
+                            spy={true} smooth={true}
+                            offset={-80}duration={700}
+                            >About</NavLink>
+
+                            <NavLink to='contact'
+                            spy={true} smooth={true}
+                            offset={-80}duration={700}
+                            >Contact</NavLink>
+                    </ul>
+                </div>
+                <ul className='social-icons'>
+                    <li>
                         <a href="https://github.com/scratchytube"><FaGithub /></a>
                     </li>
                     <li>
                         <a href="https://www.linkedin.com/in/bsahota"><FaLinkedin /></a>
                     </li>
-                </SocialIcons>
-            </NavContainer>
+                </ul>
+            </div>
         </Wrapper>
     )
 }
 
+
+
 export default Navbar
-
-const Wrapper = styled.div`
-position: sticky;
-top: 0;
-width: 100%;
-background-color: #fafafa;
-box-shadow: 0 5px 15px rgba(0, 0, 0, 0.1);
-`
-
-const NavContainer = styled.div`
-
-@media screen and (min-width: 800px) {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 1rem;
-    height: 80px;
-    z-index: 1;
-}
-`
-
-const NavHeader = styled.div`
-
-`
-
-const Logo = styled.h3`
-
-`
-
-const MobileBurger = styled.button`
-
-`
-
-const LinksContainer = styled.div`
-
-`
-
-const SocialIcons = styled.ul`
-
-`
-
-const NavUl = styled.div`
-list-style: none;
-text-align: right;
-`
 
 const NavLink = styled(Link)`
 color: black;
-font-size: 2em;
+font-size: 1.25rem;
 padding-right: 20px;
 display: inline;
 cursor: pointer;
+
+&:hover {
+
+}
 `
+
+const Wrapper = styled.nav`
+position: sticky;
+top: 0;
+width: 100%;
+height: 80px;
+background-color: #fafafa;
+z-index: 1;
+box-shadow: 0 5px 15px rgba(0, 0, 0, 0.1);
+
+.nav-header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 1.35rem;
+    letter-spacing: 0.2rem;
+    font-size: 20px;
+    text-transform: uppercase;
+}
+
+.nav-toggle {
+    color:black;
+    background: transparent;
+    border-color: transparent;
+    cursor: pointer;
+    font-size: 22px;
+}
+
+.social-icons {
+    display: none;
+}
+
+.links-container {
+    display: none;
+}
+
+
+@media screen and (min-width: 800px) {
+    .nav-container {
+        max-width: 1170px;
+        height: 80px;
+        margin: 0 auto;
+        display: flex;
+        justify-content: space-between;
+        padding: 1rem;
+    }
+
+    .links-container {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        margin-right: --22px;
+    }
+
+    .nav-header {
+        padding: 0;
+    }
+
+    .nav-toggle {
+        display: none;
+    }
+
+    .social-icons {
+        display: flex;
+        align-items: center;
+        padding-right: 1rem;
+        list-style: none;
+        font-size: 20px;
+    }
+
+    .social-icons a {
+        color: black;
+        margin: 0 0.5rem;
+        
+    }
+
+}
+
+
+`
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const NavContainer = styled.div`
+
+// @media screen and (min-width: 800px) {
+//     display: flex;
+//     justify-content: space-between;
+//     align-items: center;
+//     padding: 1rem;
+//     height: 80px;
+//     z-index: 1;
+// }
+// `
+
+// const NavHeader = styled.div`
+
+// `
+
+// const Logo = styled.h3`
+
+// `
+
+// const MobileBurger = styled.button`
+
+// `
+
+// const LinksContainer = styled.div`
+
+// `
+
+// const SocialIcons = styled.ul`
+
+// `
+
+// const NavUl = styled.div`
+// list-style: none;
+// text-align: right;
+// `
+
+
