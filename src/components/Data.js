b6e742fbf7acebaa1ab9fe4942e0824f6476be82
Brian Sahota <bpsahota@gmail.com>
2021-08-03 14:52:34 -0400

    added crypto project

--- src/components/Data.js
+++ src/components/Data.js
@@ -12,6 +12,15 @@ export const projects = [
     },
     {
         id: 2,
+        name: '$hehe Crypto',
+        description: "I made my own Crypto (i didnt') and I made a website for it.This is basically just a fun brochure site I did to sell my make-believe crypto online.",
+        image: 'https://i.imgur.com/5qQailR.png',
+        github: 'https://github.com/scratchytube/smooth_operator',
+        live: 'https://hehe-crypto.netlify.app',
+        stack: ['react', 'styled-components']
+    },
+    {
+        id: 3,
         name: 'club trader joes',
         description: "This was my capstone project for Flatiron School. Gosh I love Trader Joes and I figured this would be a fun app to build and it was. You can look at the new items that are in stores and upvote the ones you like. You can search and filter TJ recipes and you can save both to your favorites. Unfortuantely Trader Joes must have seen my presentation and overhauled their website into react, effectively unhosting all of the pictures that the TJ API was using. I've reached out to the guy who created the API and we're planning an overhaul. The landing page is cool though!",
         image: 'https://i.imgur.com/ZN0iS6O.png',
